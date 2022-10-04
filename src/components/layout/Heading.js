c5f812a6aaa01f08818809b07a4d12c6a9f96b7d
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-04 20:11:19 +0200

    Update Heading.js

--- src/components/layout/Heading.js
+++ src/components/layout/Heading.js
@@ -2,7 +2,11 @@ import PropTypes from "prop-types";
 
 export default function Heading(props) {
 	const { content } = props;
-	return <h1>{content}</h1>;
+	return (
+            <h1>
+                {content}
+            </h1>
+    );
 }
 
 Heading.propTypes = {
