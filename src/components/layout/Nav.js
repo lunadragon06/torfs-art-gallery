22af38894d398481e4df8546762168f7ef630b5b
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-04 19:23:47 +0200

    Update Nav.js

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,22 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Nav() {
 	return (
 		<nav>
-            <ul>
-                <li>
-			        <NavLink to="/">Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about">About</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/contact">Contact</NavLink>
-                </li>
-                <li>
-			        <NavLink to="/login">Login</NavLink>
-                </li>
-            </ul>
+			<Link to="/">Gallery</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
 		</nav>
 	);
 }
