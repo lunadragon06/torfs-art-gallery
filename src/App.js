6b4ad89e8683a74efe0bc8fdf9b563ede49de708
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-04 19:23:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './sass/style.scss';
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route
+} from "react-router-dom";
+import Home from "./components/pages/Home"; 
+import About from "./components/pages/About"; 
+import Contact from "./components/pages/Contact"; 
 import Footer from "./components/layout/Footer";
+import Nav from "./components/layout/Nav";
 
 function App() {
   return (
-    <>
+    <Router>
+      <Nav />
       <div className="wrapper">
-        <h1>Welcome to Torf's Gallery of Art</h1>
+        <Switch>
+        <Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/about">
+						<About />
+					</Route>
+          <Route path="/contact">
+						<Contact />
+					</Route>
+      </Switch>
       </div>
       <Footer />
-    </>
+    </Router>
   );
 }
 
