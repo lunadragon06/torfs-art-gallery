10c40168de285e8bc0a4271d80aa54adaf613f5c
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-04 19:34:36 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,9 @@
+import Heading from "../layout/Heading";
+
 export default function Home() {
-    return (
-        <h1>Home</h1>
-    );
+	return (
+		<>
+			<Heading content="Home" />
+		</>
+	);
 }
