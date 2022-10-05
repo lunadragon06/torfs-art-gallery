2b651da1f58b9c9a46fb03713381e818818eb068
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-05 11:12:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About"; 
 import Contact from "./components/pages/Contact"; 
 import Login from "./components/pages/Login"; 
+import Dashboard from "./components/pages/admin/Dashboard"; 
 import Footer from "./components/layout/Footer";
 import Nav from "./components/layout/Nav";
 
@@ -28,6 +29,9 @@ function App() {
 					</Route>
           <Route path="/login">
 						<Login />
+					</Route>
+          <Route path="/dashboard">
+						<Dashboard />
 					</Route>
         </Switch>
       </div>
