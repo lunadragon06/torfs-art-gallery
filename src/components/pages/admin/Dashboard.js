2779404552d63f1dc8bd5a4efb3eac1befc7be98
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-05 18:55:20 +0200

    Update Dashboard.js

--- src/components/pages/admin/Dashboard.js
+++ src/components/pages/admin/Dashboard.js
@@ -4,6 +4,20 @@ export default function Dashboard() {
 	return (
 		<>
 			<Heading content="Dashboard" />
+			<nav className="dashboard">
+					<p>
+						Inbox
+					</p>
+					<p>
+						Enquiries
+					</p>
+					<p>
+						Add new painting
+					</p>
+					<p>
+						Manage paintings
+					</p>
+			</nav>
 		</>
 	);
 }
