90058d7251a7729970c829f9e2c33b68774cb2ca
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-05 12:57:42 +0200

    Update Login.js

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -4,6 +4,13 @@ export default function Login() {
 	return (
 		<>
 			<Heading content="Login" />
+			<form>
+                <label htmlFor="uname">Username/email</label>
+                    <input type="text" name="uname" required />
+                <label htmlFor="psw">Password</label>
+                    <input type="password" name="psw" required />
+                <button className="loginbtn" type="submit">Login</button>
+            </form>
 		</>
 	);
 }
