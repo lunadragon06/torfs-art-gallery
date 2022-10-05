b5d3a75fb505bc8de7afc8e3b785e48073cb40b7
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-05 12:57:40 +0200

    Update Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,6 +4,21 @@ export default function Contact() {
 	return (
 		<>
 			<Heading content="Contact" />
+			<form>
+			    <label className="label" htmlFor="fname">
+					Name <span className="reqdot">*</span>
+				</label>
+                    <input className="input" type="text" name="fname" required />
+			    <label className="label" htmlFor="email">
+					Email <span className="reqdot">*</span>
+				</label>
+                    <input className="input" type="email" name="email" required />
+			    <label htmlFor="message">
+					Message <span className="reqdot">*</span>
+				</label>
+			        <textarea id="msg" name="msg"></textarea>
+                <button className="sendbtn" type="submit">Send</button>
+            </form>
 		</>
 	);
 }
