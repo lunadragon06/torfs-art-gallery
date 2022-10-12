3c3648560eb1fbce0968c8c3aac74c991509aa74
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-12 19:33:19 +0200

    Update PaintingList.js

---
+++ src/components/gallery/PaintingList.js
@@ -0,0 +1,58 @@
+import { API } from "../../constants/api";
+import axios from "axios";
+import ErrorMessage from "../../common/ErrorMessage";
+//import PaintingDetail from "./PaintingDetail";
+import PaintingItem from "./PaintingItem";
+import Heading from "../layout/Heading";
+import Loader from "../layout/Loader";
+import {useState, useEffect} from "react";
+
+const api = API;
+
+function PaintingList() {
+    const [paintings, setPainting] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(function () {
+		async function fetchPaintings() {
+			try {
+                const response = await axios.get(api);
+                if (response.status === 200) {
+                    setPainting(response.data);
+                } else {
+                    setError("A server error occured! Failed to upload paintings.");
+					console.log(setError);
+                }
+            } catch (error) {
+                setError(error.toString());
+                console.log(error);
+            } finally {
+                setLoading(false);
+            }
+		}
+		fetchPaintings();
+	}, []);
+
+    if (loading) {
+		return <Loader />;
+	}
+
+	if (error) {
+		return <ErrorMessage message={`Error: ${error}`} />;
+	}
+
+    return (
+		<>
+		    <Heading content="Gallery" />
+		    <section className="gallery">
+			    {paintings.map(function (painting) {
+				    const { id, title, image } = painting;
+				    return <PaintingItem key={id} id={id} title={title} image={image} />;
+			    })}
+		    </section>
+		</>
+    );
+}
+
+export default PaintingList; 
