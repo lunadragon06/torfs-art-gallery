8b52c541bf6452422e84c8a54442520e29e5feb8
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-12 19:43:03 +0200

    Update PaintingDetail.js

---
+++ src/components/gallery/PaintingDetail.js
@@ -0,0 +1,66 @@
+import { API } from "../../constants/api";
+import axios from "axios";
+import ErrorMessage from "../../common/ErrorMessage";
+import Heading from '../../components/layout/Heading';
+import Loader from "../layout/Loader";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function PaintingDetail() {
+	const [painting, setPainting] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+    const http = API;
+
+    let history = useNavigate();
+
+    const { id } = useParams();
+
+    if (!id) {
+        history.push("/");
+    }
+
+	useEffect(
+		function () {
+			async function fetchDataDetail() {
+				try {
+                    const response = await axios.get(http + "/" + id);
+                    console.log(response);
+                if (response.status === 200) {
+                    console.log(response);
+					setPainting(response.data);
+                } else {
+                    setError("Failed to upload this painting! Please try again later.");
+                }
+                } catch (error) {
+                    setError(error.toString());
+                } finally {
+                    setLoading(false);
+                }
+            }
+			fetchDataDetail();
+		}, 
+	);
+
+	if (loading) {
+		return <Loader />;
+	}
+	if (error) {
+		return <ErrorMessage message={`Error: ${error}`} />;
+	}
+
+	return (
+		<>
+			<section className="card">
+			    <img src={painting.image.url} alt={painting.title} />
+				<article className="painting-content">
+				    <Heading content={painting.title} />
+                    <p>Painting no. {painting.id}</p>
+				</article>
+			</section>
+		</>
+	);
+}
+
+export default PaintingDetail;
