5d7c4333c3151800a50afd072e97ce32aee1be6d
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-16 14:57:02 +0200

    Update PaintingItem.js

--- src/components/gallery/PaintingItem.js
+++ src/components/gallery/PaintingItem.js
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function PaintingItem({ id, title, image }) {
+function PaintingItem({ id, title, category, image }) {
 	return (
 		<article className="painting">
 		    <Link to={`#0`}>
 				<img src={image.url} alt={image.alternativeText} width="100%" />
 				<h2>{title}</h2> <p>Painting no. {id}</p>
+				<span>{category}</span>
 		    </Link>
 		</article>
 	);
@@ -16,6 +17,7 @@ PaintingItem.propTypes = {
 	id: PropTypes.number.isRequired,
 	image: PropTypes.any.isRequired,
 	title: PropTypes.string.isRequired,
+	category: PropTypes.string.isRequired, 
 };
 
 export default PaintingItem;
