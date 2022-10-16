7fce5b4e5cdcd01c146cee772dc240542c9f6c74
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-16 16:24:16 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,6 +4,12 @@ export default function Home() {
 	return (
 		<>
 			<Heading content="Home" />
+			<div className="searchcont">
+			    <input className="searchbar" 
+			           type="text" 
+				       placeholder="Search for paintings .." 
+			    />
+			</div>
 		</>
 	);
 }
