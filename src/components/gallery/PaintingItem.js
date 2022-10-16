36e4678893af6bb3c73535e6a315566711652015
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-16 14:50:51 +0200

    Update PaintingItem.js

--- src/components/gallery/PaintingItem.js
+++ src/components/gallery/PaintingItem.js
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function PaintingItem({ id, title, image, alt }) {
+function PaintingItem({ id, title, image }) {
 	return (
 		<article className="painting">
 		    <Link to={`#0`}>
-				<img src={image.url} alt={alt} width="100%" />
+				<img src={image.url} alt={image.alternativeText} width="100%" />
 				<h2>{title}</h2> <p>Painting no. {id}</p>
 		    </Link>
 		</article>
