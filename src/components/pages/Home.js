325aad76cc24acbed3406762e9253c768d9f238d
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-16 17:16:14 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,30 +5,40 @@ import Heading from "../layout/Heading";
 export default function Home() {
 	const [paintings, setPaintings] = useState([]);
 	const [text, setText] = useState('');
+	const [suggestions, setSuggestions] = useState([])
 	useEffect(() => {
 		const loadPaintings = async()=> {
 			const response = await axios.get('https://torfs-art-gallery.herokuapp.com/products')
-			//console.log(response.data)
 			setPaintings(response.data)
 		}
 		loadPaintings();
 
 	}, [])
 	const onChangeHandler = (text)=> {
-		setText(text)
+		let matches = []
+		if (text.length>0) {
+			matches = paintings.filter(paint=> {
+				return paint.title.match(text)
+			})
+		}
+		console.log('matches', matches)
+		setSuggestions(matches)
+		setText(text);
 	}
 
 	return (
 		<>
 			<Heading content="Home" />
 			<div className="searchcont">
-				<div>{text}</div>
 			    <input className="searchbar" 
 			           type="text" 
 				       placeholder="Search for paintings .."
 					   onChange={e=>onChangeHandler(e.target.value)} 
 					   value={text}
 			    />
+				{suggestions && suggestions.map((suggestions, i) =>
+				<div className="results" key={i}>{suggestions.title}</div>
+				)}
 			</div>
 		</>
 	);
