30fc35362d4d00e997b65ae928cf3f35fc0cbdff
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-16 15:56:06 +0200

    Update PaintingItem.js

--- src/components/gallery/PaintingItem.js
+++ src/components/gallery/PaintingItem.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function PaintingItem({ id, title, category, image }) {
 	return (
 		<article className="painting">
-		    <Link to={`detail/${id}`}>
+		    <Link to={`painting/${id}`}>
 				<img src={image.url} alt={image.alternativeText} width="100%" />
 				<h2>{title}</h2> <p>Painting no. {id}</p>
 				<span>{category}</span>
