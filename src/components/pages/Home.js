2203dd4017125b8352ec1b5f84caf0e53064e7b9
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-17 20:18:45 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,68 +1,12 @@
-import { API } from "../../constants/api";
-import axios from "axios";
-import { Link } from "react-router-dom";
 import Heading from "../layout/Heading";
-import { FaSearch } from 'react-icons/fa';
-import { useEffect, useState } from "react";
-
-const results = API;
+import Searchbar from "../layout/Searchbar";
 
 export default function Home() {
-	const [paintings, setPaintings] = useState([]);
-	const [text, setText] = useState('');
-	const [suggestions, setSuggestions] = useState([]);
-
-	useEffect(() => {
-		const loadPaintings = async()=> {
-			const response = await axios.get(results);
-			setPaintings(response.data);
-		}
-		loadPaintings();
-
-	}, [])
-	const onSuggestHandler = (text)=> {
-		setText(text);
-		setSuggestions([]);
-	}
-	const onChangeHandler = (text)=> {
-		let matches = []
-		if (text.length>0) {
-			matches = paintings.filter(paint=> {
-				return paint.title.match(text);
-			})
-		}
-		setSuggestions(matches);
-		setText(text);
-	}
 
 	return (
 		<>
 			<Heading content="Home" />
-			<div className="searchcont">
-			    <FaSearch className="icon"
-                          style={{
-						  left: '16px',
-                          position: 'relative',
-						  top: '54px',
-                }} />
-			    <input className="searchbar" 
-			           type="text" 
-				       placeholder="Search for paintings .."
-					   onChange={e=>onChangeHandler(e.target.value)} 
-					   value={text} 
-					   onBlur={()=>{ 
-						setTimeout(() => {
-							setSuggestions([])
-						}, 100);
-					   }}
-			    />
-				{suggestions && suggestions.map((suggestion, i) =>
-				    <Link to ={`painting/${suggestion.id}`} className="result" key={i} 
-				          onClick={() => onSuggestHandler(suggestion.title)}>
-						    {suggestion.title}
-				    </Link>
-				)}
-			</div>
+			<Searchbar />
 		</>
 	);
 }
