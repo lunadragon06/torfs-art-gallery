245619aba150adc4fac26ffb01fd509a2dbe0992
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-18 22:44:54 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import Heading from "../layout/Heading";
 import Searchbar from "../layout/Searchbar";
+import Slider from "../slider/Slider";
 
 export default function Home() {
 
 	return (
 		<>
 			<Heading content="Home" />
+			<Slider />
 			<Searchbar />
 		</>
 	);
