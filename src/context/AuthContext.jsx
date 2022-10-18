24a1e02966c6aa2370767503d769b92d1c832ae3
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-18 22:45:01 +0200

    Create AuthContext.jsx

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import useLocalStorage from "../hooks/localStorage";
+
+const AuthContext = React.createContext([null, () => { }]);
+/**
+ * 
+ * @param {*} props 
+ * @returns the authentication key
+ */
+export const AuthProvider = (props) => {
+    const [auth, setAuth] = useLocalStorage("auth", null);
+    return <AuthContext.Provider value={[auth, setAuth]}>
+                {props.children}
+           </AuthContext.Provider>;
+};
+
+export default AuthContext;
