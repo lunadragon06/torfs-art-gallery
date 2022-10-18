ab49e47e97015524997a17418d0692ff073a73c6
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-18 12:15:52 +0200

    Update PaintingItem.js

--- src/components/gallery/PaintingItem.js
+++ src/components/gallery/PaintingItem.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function PaintingItem({ id, title, category, image }) {
+function PaintingItem({ id, image }) {
 	return (
 		    <span>
 		            <Link className="link" to={`painting/${id}`}>
