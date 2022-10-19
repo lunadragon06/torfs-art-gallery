a8f663adf03e99adadf982a95fa769708fc2d4ad
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-19 21:01:05 +0200

    Update Nav.js

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,19 +1,63 @@
+//import AuthContext from "../../context/authContext";
 import { Link } from "react-router-dom";
+/*
+import {  NavLink, useNavigate  } from "react-router-dom";
+import { useContext } from "react";
+*/
 
 function Nav() {
+    /* 
+    const [auth, setAuth] = useContext(AuthContext);
+
+	const history = useNavigate();
+
+	function logout() {
+		setAuth(null);
+		history("/");
+	}
+
+    const onLinkClick = () => {
+        document.querySelector("#nav").checked = false; 
+    };
+  
+    document
+        .querySelectorAll("a")
+        .forEach((element) => element.addEventListener("click", onLinkClick));
+    */
+
 	return (
 		<nav>
             <a className="wordmark" href="/">
                 Torf's Gallery of Art
             </a>
             <ul>
-			    <Link to="/">Home</Link>
-                <Link to="/gallery">Gallery</Link>
-                <Link to="/about">About</Link>
+                <li>
+			        <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/gallery">Gallery</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
                 <Link to="/contact">Contact</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/dashboard">Dashboard</Link>
-                <Link to="/add">Add painting</Link>
+                </li>
+                {/* {auth ? (
+				<> */}
+                <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                </li>
+                {/* <li onClick={logout} className="logbtn">
+                        <Link to="/" onClick="onLinkClick()">Logout</Link>
+                </li> 
+                </>
+                ) : (
+                */}
+                <li className="logbtn">
+                    <Link to="/login">Login</Link>
+                </li>
+                {/* )} */}
             </ul>
 		</nav>
 	);
