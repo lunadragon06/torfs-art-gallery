cd4a508cf5e5f67f86ee4969cc203c40a6591207
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-19 21:01:22 +0200

    Update localStorage.js

--- src/hooks/localStorage.js
+++ src/hooks/localStorage.js
@@ -1,27 +1,29 @@
 import { useState } from "react";
 
 export default function useLocalStorage(key, initialValue) {
+	const [storedValue, setStoredValue] = useState(() => {
+		try {
+			const item = window.localStorage.getItem(key);
+			return item ? JSON.parse(item) : initialValue;
+            
+		} catch (error) {
+			console.log(error);
+			return initialValue;
+		}
+	});
 
-    const [storedValue, setStoredValue] = useState(() => {
-        try {
-            const item = window.localStorage.getItem(key);
-            return item ? JSON.parse(item) : initialValue;
-        } catch (error) {
-            console.log(error);
-            return initialValue;
-        }
-    });
+	const setValue = (value) => {
+		try {
+			const valueToStore = value instanceof Function ? value(storedValue) : value;
 
-    const setValue = (value) => {
-        try {
-            const valueToStore = value instanceof Function ? value(storedValue) : value;
-            setStoredValue(valueToStore);
+			setStoredValue(valueToStore);
 
-            window.localStorage.setItem(key, JSON.stringify(valueToStore));
-        } catch (error) {
-            console.log(error);
-        }
-    };
+			window.localStorage.setItem(key, JSON.stringify(valueToStore));
 
-    return [storedValue, setValue];
-}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return [storedValue, setValue];
+    
