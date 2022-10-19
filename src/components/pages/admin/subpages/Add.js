50eab0bb0d54a2e90d7d24661cd95430831a5a2e
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-19 21:21:45 +0200

    Update Add.js

--- src/components/pages/admin/subpages/Add.js
+++ src/components/pages/admin/subpages/Add.js
@@ -1,6 +1,60 @@
+/* 
+import * as yup from "yup";
+import FormError from "../../components/common/FormError"; 
+*/
 import Heading from "../../../layout/Heading";
+/*
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import useAxios from "../../components/hooks/useAxios";
+import { yupResolver } from "@hookform/resolvers/yup";
+*/
+
+/* 
+const schema = yup.object().shape({
+    title: yup.string().required("A title name for your painting is required"),
+    description: yup.string().required("Description for your painting is required"),
+    files: yup.mixed()
+});
+*/
 
 export default function Add() {
+	/* 
+	const [submitting, setSubmitting] = useState(false);
+    const [serverError, setServerError] = useState(null);
+
+    const history = useNavigate();;
+    const http = useAxios();
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+    });
+    async function onSubmit(inputData) {
+        setSubmitting(true);
+        setServerError(null);
+
+		inputData.status = "publish";
+
+		const formData = new FormData();
+        for (const image of inputData.files) {
+            formData.append('files.images', image);
+        }
+
+		const { image, ...data } = inputData;
+        formData.append("data", JSON.stringify(data));
+        try {
+			const response = await http.post("/products", formData)
+			    history("/dashboard");
+        } catch (error) {
+            console.log("error", error);
+            setServerError(error.toString());
+        } finally {
+            setSubmitting(false);
+        }
+    }
+	*/
 	return (
 		<>
 			<Heading content="Add" />
