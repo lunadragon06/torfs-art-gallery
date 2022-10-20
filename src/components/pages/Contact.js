ddb8c068ad15e0aabaf7ffdbacfe7c99a9bb0ffc
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-20 13:19:47 +0200

    Update Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -18,19 +18,19 @@ export default function Contact() {
 			        <label className="label" htmlFor="fname">
 					    Name <span className="reqdot">*</span>
 				    </label>
-                    <input className="input" type="text" name="fname" required />
+                    <input className="input" type="text" name="first_name" required />
 				</div>
 				<div>
 			        <label className="label" htmlFor="email">
 					    Email <span className="reqdot">*</span>
 				    </label>
-                    <input className="input" type="email" name="email" required />
+                    <input className="input" type="email" name="user_email" required />
 				</div>
 				</section>
 			    <label htmlFor="message">
 					Message <span className="reqdot">*</span>
 				</label>
-			        <textarea id="msg" name="msg" required></textarea>
+			        <textarea id="msg" name="message" required></textarea>
 				<input type="submit" value="Send" className="sendbtn" />
             </form>
 		</>
