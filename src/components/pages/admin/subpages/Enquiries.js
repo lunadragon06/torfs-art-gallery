a22f3f6d15a0b99fee25bde937e5dbcf19643bde
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-24 15:29:14 +0200

    Update Enquiries.js

--- src/components/pages/admin/subpages/Enquiries.js
+++ src/components/pages/admin/subpages/Enquiries.js
@@ -1,8 +1,15 @@
 import Heading from "../../../layout/Heading";
+import { Link } from "react-router-dom";
 
 export default function Enquiries() {
 	return (
 		<>
+			<div className="breadcrumb">
+			    <Link to="/dashboard">
+				    Dashboard
+			    </Link>
+			    <span className="current-link"> / <b> Enquiries</b></span>
+		    </div>
 			<Heading content="Enquiries" />
 		</>
 	);
