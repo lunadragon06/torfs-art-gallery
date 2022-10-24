b6542c83e369946b0c9ccea2c19d272cf5b99081
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-24 18:16:38 +0200

    Update useLocalStorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -15,7 +15,9 @@ export default function useLocalStorage(key, initialValue) {
 	const setValue = (value) => {
 		try {
 			const valueToStore = value instanceof Function ? value(storedValue) : value;
+
 			setStoredValue(valueToStore);
+
 			window.localStorage.setItem(key, JSON.stringify(valueToStore));
 
 		} catch (error) {
