a14125ccbbaa3ab1ba1ecd73545b3d5ff08239af
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-24 15:29:17 +0200

    Update Inbox.js

--- src/components/pages/admin/subpages/Inbox.js
+++ src/components/pages/admin/subpages/Inbox.js
@@ -1,8 +1,15 @@
 import Heading from "../../../layout/Heading";
+import { Link } from "react-router-dom";
 
 export default function Inbox() {
 	return (
 		<>
+			<div className="breadcrumb">
+			<Link to="/dashboard">
+				Dashboard
+			</Link>
+			<span className="current-link"> / <b> Inbox</b></span>
+		    </div>
 			<Heading content="Inbox" />
         </>
 	);
