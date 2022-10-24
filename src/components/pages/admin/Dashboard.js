70c6afe48106295ba98897bed5dd9c1d39251677
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-24 15:29:11 +0200

    Update Dashboard.js

--- src/components/pages/admin/Dashboard.js
+++ src/components/pages/admin/Dashboard.js
@@ -1,9 +1,29 @@
+import { Link } from "react-router-dom";
 import Heading from "../../layout/Heading";
+import React from 'react';
 
-export default function Dashboard() {
-	return (
-		<>
-			<Heading content="Dashboard" />
-		</>
-	);
+function AdminMenu() {
+    return ( 
+        <>
+            <Heading content="Dashboard" />
+            <nav className="adminav">
+                <ul className="adminav__inner">
+                    <li className="adminav__item">
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li className="adminav__item">
+                        <Link to="/inbox">Inbox</Link>
+                    </li>
+                    <li className="adminav__item">
+                        <Link to="/enquiries">Enquiries</Link>
+                    </li>
+                    <li className="adminav__item">
+                        <Link to="/add">Add painting</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
 }
+
+export default AdminMenu;
