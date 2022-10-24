a511b44116ec4bbe51fe675b02c519697c02b724
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-24 12:44:45 +0200

    Update Dashboard.js

--- src/components/pages/admin/Dashboard.js
+++ src/components/pages/admin/Dashboard.js
@@ -1,11 +1,9 @@
-import AdminMenu from "./AdminMenu";
 import Heading from "../../layout/Heading";
 
 export default function Dashboard() {
 	return (
 		<>
 			<Heading content="Dashboard" />
-			<AdminMenu />
 		</>
 	);
 }
