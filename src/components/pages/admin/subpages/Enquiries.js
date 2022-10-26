c585c20f49b1f91c498f8be578a6215b73f8f949
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-26 19:25:44 +0200

    Update Enquiries.js

--- src/components/pages/admin/subpages/Enquiries.js
+++ src/components/pages/admin/subpages/Enquiries.js
@@ -1,7 +1,43 @@
+import FormError from "../../../../common/FormError";
 import Heading from "../../../layout/Heading";
 import { Link } from "react-router-dom";
+import Loader from "../../../layout/Loader";
+import React from 'react';
+import useAxios from "../../../../hooks/useAxios";
+import { useState, useEffect } from "react";
 
-export default function Enquiries() {
+function Enquiries() {
+	const [enquiries, setEnquiries] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const http = useAxios();
+
+    useEffect(function () {
+
+        async function showEnquiries() {
+            try {
+                const response = await http.get("/enquiries");
+                setEnquiries(response.data);
+
+            } catch (error) {
+                console.log("error", error);
+                setError(error.toString());
+            } finally {
+                setLoading(false);
+            }
+        }
+        showEnquiries();
+    }, [http]);
+	
+    if (loading) {
+		return <Loader />;
+	}
+
+    if (error) return <FormError>
+		                {"Unable to upload enquiry requests at the moment."}
+		              </FormError>;
+
 	return (
 		<>
 			<div className="breadcrumb">
@@ -11,7 +47,21 @@ export default function Enquiries() {
 			    <span className="current-link"> / <b> Enquiries</b></span>
 		    </div>
 			<Heading content="Enquiries" />
-			
+			<section>
+			{enquiries.map((enquiry) => {
+                            return (
+                                <div key={enquiry.id}>
+                                        <p>{enquiry.created_at.slice(0, -5)}</p>
+                                        <p>{enquiry.name}</p>
+                                        <p>{enquiry.mail}</p>
+										<p>{enquiry.subject}</p>
+                                        <p>{enquiry.note}</p>
+                                </div>
+                            );
+                        })}
+			</section>
 		</>
 	);
 }
+
+export default Enquiries;
