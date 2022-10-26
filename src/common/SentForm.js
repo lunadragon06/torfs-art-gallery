8e222533dcf7c49a10f5666b5d2ff3852af780c3
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-26 12:56:18 +0200

    Create SentForm.js

---
+++ src/common/SentForm.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+function SentForm() {
+    return (
+        <div className="message-sent">
+            The message has been sent with success! 
+            Your request will be replied as soon as possible. 
+        </div>
+    )
+}
+
+export default SentForm;
