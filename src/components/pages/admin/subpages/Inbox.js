2446ad9bee4af078380f8008ad068ecf395a5dde
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-26 19:43:30 +0200

    Update Inbox.js

--- src/components/pages/admin/subpages/Inbox.js
+++ src/components/pages/admin/subpages/Inbox.js
@@ -1,8 +1,46 @@
+import AuthContext from "../../../../context/AuthContext";
+import FormError from "../../../../common/FormError";
 import Heading from "../../../layout/Heading";
 import { Link } from "react-router-dom";
+import Loader from "../../../layout/Loader";
+import React from 'react';
+import useAxios from "../../../../hooks/useAxios";
+import { useState, useEffect, useContext } from "react";
 
 function Inbox() {
-	
+	const [auth] = useContext(AuthContext);
+	console.log(auth);
+    const [contacts, setContacts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const http = useAxios();
+
+    useEffect(function () {
+        async function showContact() {
+            try {
+                const response = await http.get("/inbox");
+                setContacts(response.data);
+
+            } catch (error) {
+                console.log("error", error);
+                setError(error.toString());
+
+            } finally {
+                setLoading(false);
+            }
+        }
+        showContact();
+    }, [http]);
+
+	if (loading) {
+		return <Loader />;
+	}
+
+    if (error) return <FormError>
+	                        {"Unable to upload messages at the moment."}
+                      </FormError>;
+
 	return (
 		<>
 			<div className="breadcrumb">
@@ -12,7 +50,18 @@ function Inbox() {
 			<span className="current-link"> / <b> Inbox</b></span>
 		    </div>
 			<Heading content="Inbox" />
-
+			<section>
+			{contacts.map((contact) => {
+                            return (
+                                <div key={contact.id}>
+                                        <p>{contact.created_at.slice(0, -5)}</p>
+                                        <p>{contact.name}</p>
+                                        <p>{contact.email}</p>
+                                        <p>{contact.message}</p>
+                                </div>
+                            );
+                        })}
+			</section>
         </>
 	);
 }
