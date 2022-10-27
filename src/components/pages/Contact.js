65d2001c751925c167914eddbbe85968c09455b2
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-27 18:55:10 +0200

    Update Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,45 +1,100 @@
-import emailjs from "emailjs-com"; 
+import * as yup from "yup";
+import axios from "axios";
+import { BASE_URL } from "../../constants/data";
+import FormError from "../../common/FormError";
 import Heading from "../layout/Heading";
+import React, { useState } from 'react';
+import SentForm from "../../common/SentForm";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-export default function Contact() {
-	function sendEmail(e) {
-		e.preventDefault();
+const url = BASE_URL + "/inbox";
 
-		emailjs.sendForm(
-			"service_k7mcnpl", 
-		    "template_yjqrb65",
-			e.target, 
-			"jiyj71ORX3nS0yNsf"
-		).then(res=> {
-			alert("Your message has been sent!");
-			console.log(res);
-		}).catch(err=> console.log(err));  
-	}
+const schema = yup.object().shape({
+    name: yup.string()
+            .required("You must enter your name."),
+    email: yup.string()
+            .email("The email is not valid.")
+            .required("Please enter your email."),
+    message: yup.string()
+            .required("Please enter your message here.")
+            .min(15, "Your message must contain at least 15 characters long."),
+});
+
+function Contact() {
+    const [submitting, setSubmitting] = useState(false);
+    const [submitError, setSubmitError] = useState(null);
+    const [formSentMessage, setFormSentMessage] = useState(false);
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
+
+    async function onSubmit(data) {
+        setSubmitting(true);
+        setSubmitError(null);
+
+        try {
+            const response = await axios.post(url, data);
+            console.log(response);
+            setFormSentMessage(true);
+
+            reset({
+                name: "",
+                email: "",
+                message:""
+            });
+        } catch (error) {
+            console.log("error", error);
+            setSubmitError(error.toString());
+        } finally {
+            setSubmitting(false);
+        }
+    }
 
 	return (
 		<>
 			<Heading content="Contact" />
-			<form onSubmit={sendEmail}>
+            {submitError && <FormError>{submitError}</FormError>}
+            {formSentMessage && <SentForm></SentForm>}
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<section className="rowform">
 				<div>
 			        <label className="label" htmlFor="fname">
 					    Name <span className="reqdot">*</span>
 				    </label>
-                    <input className="input" type="text" name="first_name" required />
+                    <input className="input" 
+                           type="text" 
+                           name="first_name"
+                           {...register("name")} />
+                    {errors.name && <FormError>{errors.name.message}</FormError>}
 				</div>
 				<div>
 			        <label className="label" htmlFor="email">
 					    Email <span className="reqdot">*</span>
 				    </label>
-                    <input className="input" type="email" name="user_email" required />
+                    <input className="input" 
+                           type="email" 
+                           name="user_email" 
+                           {...register("email")} />
+                    {errors.email && <FormError>{errors.email.message}</FormError>}
 				</div>
 				</section>
 			    <label htmlFor="message">
 					Message <span className="reqdot">*</span>
 				</label>
-			        <textarea id="msg" name="message" required></textarea>
-				<input type="submit" value="Send" className="sendbtn" />
+			        <textarea id="msg" 
+                              name="message" 
+                              {...register("message")}>
+                    </textarea>
+                    {errors.message && <FormError>{errors.message.message}</FormError>}
+				<button type="submit" 
+                        className="sendbtn">
+                            {submitting ? "Sending..." : "SEND"}
+                </button>
             </form>
 		</>
 	);
 }
+
+export default Contact;
