80ba79bb8db99cbd49ff7452fc3aa32b5cd384c7
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-10-29 21:57:09 +0200

    Update Nav.js

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,4 +1,5 @@
 import AuthContext from "../../context/AuthContext";
+import { HiOutlineMenuAlt4 } from 'react-icons/hi';
 import { Link, useHistory } from "react-router-dom";
 import { useContext } from "react";
 
@@ -14,36 +15,40 @@ function Nav() {
 	}
 
 	return (
-		<nav>
-            <a id="logo" href="/">
+		    <nav className="main-nav">
+            <input type="checkbox" id="check" />
+            <label htmlFor="check" className="menu-btn">
+                <HiOutlineMenuAlt4 size={40} color='aliceblue' />
+            </label>
+            <a className="logo" href="/">
                 Torf's Gallery of Art
             </a>
-            <ul id="nav-bar">
-                <li>
-                    <Link to="/gallery">Gallery</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-                {auth ? (
-				<>
-                <li>
-                    <Link to="/dashboard">Dashboard</Link>
-                </li>
-                <li>
-                    <button onClick={logout}>Logout</button>
-                </li> 
-                </>
-                ) : (
-                <li className="logbtn">
-                    <Link to="/login">Login</Link>
-                </li>
-                )}
-            </ul>
-		</nav>
+                <ul className="navlinks">
+                    <li>
+                        <Link to="/gallery">Gallery</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    {auth ? (
+				    <>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                        <button onClick={logout}>Logout</button>
+                    </li> 
+                    </>
+                    ) : (
+                    <li className="logbtn">
+                        <Link to="/login">Login</Link>
+                    </li>
+                    )}
+                </ul>
+		    </nav>
 	);
 }
 
