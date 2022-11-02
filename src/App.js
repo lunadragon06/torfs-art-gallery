de88b55c0db8a9f86a7a833131a4c37662159e07
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-11-02 10:34:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 import Home from "./components/pages/Home"; 
 import About from "./components/pages/About"; 
 import Contact from "./components/pages/Contact"; 
-import Login from "./components/pages/Login"; 
+import Login from "./components/pages/admin/Login"; 
 import Footer from "./components/layout/Footer";
 import Nav from "./components/layout/Nav";
 import PaintingDetail from "./components/gallery/PaintingDetail"; 
