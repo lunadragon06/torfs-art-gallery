0519c4e945ac96409425628201ae9c40fed17925
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-11-02 10:34:08 +0100

    Create Heading.js

---
+++ src/components/layout/typography/Heading.js
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+
+export default function Heading(props) {
+	const { content } = props;
+	return (
+            <h1>
+                {content}
+            </h1>
+    );
+}
+
+Heading.propTypes = {
+	content: PropTypes.string.isRequired,
+};
