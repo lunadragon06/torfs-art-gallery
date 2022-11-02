9c994fba78998d5e027fa6fb404ed279a15c06fa
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-11-02 13:01:54 +0100

    Update Dashboard.js

--- src/components/pages/admin/Dashboard.js
+++ src/components/pages/admin/Dashboard.js
@@ -1,3 +1,7 @@
+import { TfiDashboard } from 'react-icons/tfi';
+import { TfiEmail } from 'react-icons/tfi';
+import { TfiList } from 'react-icons/tfi';
+import { TfiPlus } from 'react-icons/tfi';
 import { Link } from "react-router-dom";
 import Heading from "../../layout/typography/Heading";
 import React from 'react';
@@ -7,18 +11,27 @@ function AdminMenu() {
         <>
             <Heading content="Dashboard" />
             <nav className="adminav">
+                <h2 style={{ fontWeight: 'bold', margin: '-.25rem 2rem .75rem 2rem', }}>Settings</h2>
                 <ul className="adminav__inner">
                     <li className="adminav__item">
-                        <Link to="/dashboard">Dashboard</Link>
+                        <Link to="/dashboard" className="adminav_link" id="active">
+                            <TfiDashboard style={{ margin: '0 .75rem 0', }} /> Dashboard
+                        </Link>
                     </li>
-                    <li className="adminav__item">
-                        <Link to="/inbox">Inbox</Link>
+                    <li className="adminav__item" id="sublink">
+                        <Link to="/inbox" className="adminav_link">
+                            <TfiEmail style={{ margin: '0 .75rem 0', }} /> Inbox
+                        </Link>
                     </li>
-                    <li className="adminav__item">
-                        <Link to="/enquiries">Enquiries</Link>
+                    <li className="adminav__item" id="sublink">
+                        <Link to="/enquiries" className="adminav_link">
+                            <TfiList style={{ margin: '0 .75rem 0', }} /> Enquiries
+                        </Link>
                     </li>
-                    <li className="adminav__item">
-                        <Link to="/add">Add painting</Link>
+                    <li className="adminav__item" id="sublink">
+                        <Link to="/add" className="adminav_link">
+                            <TfiPlus style={{ margin: '0 .75rem 0', }} /> Add painting
+                        </Link>
                     </li>
                 </ul>
             </nav>
