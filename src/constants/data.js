d01c3df90e7b17875d8077fa870f43929453853c
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-11-05 13:05:14 +0100

    Update data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://torfs-art-gallery.herokuapp.com";
+// process.env.REACT_APP_BASE_URL
+export const BASE_URL = "http://localhost:8082";
 export const TOKEN_PATH = "/auth/local";
