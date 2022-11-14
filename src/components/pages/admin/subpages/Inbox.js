467cd592e28fc075497cc2175a22b3cdfc0b9ed0
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-11-14 20:54:36 +0100

    Update Inbox.js

--- src/components/pages/admin/subpages/Inbox.js
+++ src/components/pages/admin/subpages/Inbox.js
@@ -4,6 +4,7 @@ import Heading from "../../../layout/typography/Heading";
 import { HiArrowLeft } from 'react-icons/hi'; 
 import { Link, useHistory } from "react-router-dom";
 import Loader from "../../../layout/Loader";
+import moment from "moment/moment";
 import React from 'react';
 import useAxios from "../../../../hooks/useAxios";
 import { useState, useEffect, useContext } from "react";
@@ -54,14 +55,24 @@ function Inbox() {
                 <Link to="/dashboard" className="view" style={{ display: 'inline-block', marginBottom: '2.5rem' }}><HiArrowLeft /> Back to dashboard</Link>
 		    </div>
 			<Heading content="Inbox" />
-			<section>
-			{contacts.map((contact) => {
+			<section style={{ margin: '3rem auto 0', maxWidth: '1000px', }}>
+			{contacts.sort( (a,b) => a.id > b.id ? 1 : -1 ).reverse().map((contact) => {
                             return (
-                                <article key={contact.id}>
-                                        <p>{contact.created_at.slice(0, -5)}</p>
-                                        <p>{contact.first_name}</p>
-                                        <p>{contact.email}</p>
-                                        <p>{contact.message}</p>
+                                <article className="inbox-msg" key={contact.id}>
+                                    <div className="inbox-detail">
+                                        <p>
+                                            <b>{contact.first_name}</b> ({contact.email})
+                                        </p>
+                                        <p>  
+                                        {moment(contact.created_at).format("ddd, DD.MM.YYYY.")}
+                                            <span className="post-time">
+                                                {moment(contact.created_at).format(" (HH:mm)")}
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div className="message-box">
+                                        <p>{contact.message.substring(0, 250)} ...</p>
+                                    </div>
                                 </article>
                             );
                         })}
