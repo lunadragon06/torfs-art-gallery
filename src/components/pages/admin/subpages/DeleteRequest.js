f0ce9bcc4d594b97e6bd660cb6a72309bb8fab12
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-11-16 14:32:23 +0100

    Create DeleteRequest.js

---
+++ src/components/pages/admin/subpages/DeleteRequest.js
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import useAxios from "../../../../hooks/useAxios";
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+
+export default function DeleteRequest({ id }) {
+	const [error, setError] = useState(null);
+
+	const http = useAxios();
+	const history = useHistory();
+
+	const url = `/requests/${id}`;
+
+	async function handleDelete() {
+        const confirmDelete = window.confirm("Delete this message?");
+
+        if (confirmDelete) {
+		try {
+			await http.delete(url);
+			history.push("/dashboard");
+		} catch (error) {
+			setError(error);
+		}
+	  }
+    }
+
+	return (
+		<button type="button" className="delbtn" onClick={handleDelete}>
+			{error ? "Error" : "Delete"}
+		</button>
+	);
+}
+
+DeleteRequest.propTypes = {
+	id: PropTypes.number.isRequired,
+};
