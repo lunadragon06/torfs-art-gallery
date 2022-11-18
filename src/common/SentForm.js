deb1cf6b488c25a2f68218e26223bc4527bebb99
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-11-18 22:06:09 +0100

    Update SentForm.js

--- src/common/SentForm.js
+++ src/common/SentForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 function SentForm() {
     return (
         <div className="message-sent">
-            The message has been sent! Your message will be replied as soon as possible. 
+            Your message has been sent and will be replied as soon as possible! 
         </div>
     )
 }
