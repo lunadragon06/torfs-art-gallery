26ff73a6660f299365dc4b62d6d16249f7337c7c
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-11-20 04:01:46 +0100

    Update data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -8,7 +8,7 @@ export const API = "/paintings";
 export const BASE_URL = "https://torfs-art-gallery.herokuapp.com";
 
 // For sliders 
-export const HEADER = "/sliders"
+export const SLIDER = "/sliders"
 
 // Admin 
 export const TOKEN_PATH = "/auth/local"; 
