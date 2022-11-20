eacfb6d8a76d9fde3b766584ccd4c4bd257db7d8
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-11-20 04:00:55 +0100

    Update data.js

--- src/constants/data.js
+++ src/constants/data.js
@@ -4,5 +4,11 @@ REACT_APP_BASE_URL=https://torfs-art-gallery.herokuapp.com
 export const BASE_URL = process.env.REACT_APP_BASE_URL; 
 */
 
+export const API = "/paintings";
 export const BASE_URL = "https://torfs-art-gallery.herokuapp.com";
+
+// For sliders 
+export const HEADER = "/sliders"
+
+// Admin 
 export const TOKEN_PATH = "/auth/local"; 
